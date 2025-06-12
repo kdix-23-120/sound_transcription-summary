de4c5dcac3a6ea7a65b3325c0dc389aa88c7079b
kdix-23-120 <166372831+kdix-23-120@users.noreply.github.com>
2025-06-12 16:04:26 +0900

    Update server.js
    add  cors

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const cors = require('cors');
 const path = require("path");
 const app = express();
 app.use(express.static("public"));
+app.use(cors());
 app.listen(3000, () => console.log("App: http://localhost:3000"));
